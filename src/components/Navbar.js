618ab16c6f0606e5161d7a211431ed40f7064001
Muhammad Yahiya <47945063+muhammadyahiya@users.noreply.github.com>
2023-02-23 21:24:24 +0530

    Revert "customized the main page"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
               to="headerbg"
               style={{ cursor: "pointer" }}
             >
-              Optimum AI
+              DarkLeas
             </Link>
           </h1>
           <ul className="bar">
